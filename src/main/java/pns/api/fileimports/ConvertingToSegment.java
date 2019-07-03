df9bb16dd1f25574badb745800ead3b14957fd5f
Movement <Movement@192.168.1.22>
2019-07-03 18:38:29 +0300

    Refractoring

--- src/main/java/pns/api/fileimports/ConvertingToSegment.java
+++ src/main/java/pns/api/fileimports/ConvertingToSegment.java
@@ -16,7 +16,7 @@ public class ConvertingToSegment {
 
     public Segment convert(String data) {
         System.out.println("Converting!!");
-        double v1 = 0;
+        double a1 = 0, a2 = 0, a3 = 0, v1 = 0, v2 = 0, v3 = 0;
         double mmoment = 0;
         Segment segment = new Segment();
         data = data.replace(',', '.');
@@ -28,25 +28,33 @@ public class ConvertingToSegment {
             Point9 ptPoint9 = new Point9();
             if (s.length() > 0) {
                 try {
-                    try {
-                        mmoment = Double.parseDouble(ss[0].trim());
-                        ptPoint9.setMoment(mmoment);
-                    } catch (NumberFormatException e) {
-                        System.out.println("MM " + k + "     mmoment " + mmoment + "  ss[0] " + ss[0]);
-                    }
-                    try {
-                        v1 = Double.parseDouble(ss[1].trim());
-                        ptPoint9.setV1(v1);
-                        //  System.out.println("k=" + k + "   " + ptPoint9);
-                    } catch (NumberFormatException e) {
-                        System.out.println("   VV   " + k + "  VV  " + v1 + "  ss[1] " + ss[1]);
-                    }
-                    k++;
-                    segment.getPoint9TreeSet().add(ptPoint9);
-
+                    mmoment = Double.parseDouble(ss[0].trim());
+                    ptPoint9.setMoment(mmoment);
+                } catch (NumberFormatException e) {
+                    System.out.println("MM " + k + "     mmoment " + mmoment + "  ss[0] " + ss[0]);
                 } catch (ArrayIndexOutOfBoundsException e) {
                 }
+                try {
+                    v1 = Double.parseDouble(ss[1].trim());
+                    ptPoint9.setV1(v1);
+                    //  System.out.println("k=" + k + "   " + ptPoint9);
+                } catch (NumberFormatException e) {
+                    System.out.println("   V   " + k + "  VV  " + v1 + "  ss[1] " + ss[1]);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                }
+                try {
+                    a1 = Double.parseDouble(ss[2].trim());
+                    ptPoint9.setA1(a1);
+                    //  System.out.println("k=" + k + "   " + ptPoint9);
+                } catch (NumberFormatException e) {
+                    System.out.println("   A   " + k + "  VV  " + a1 + "  ss[1] " + ss[1]);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                }
+                k++;
+                segment.getPoint9TreeSet().add(ptPoint9);
+
             }
+            System.out.println("     Converting   DONE! " + segment.getPoint9TreeSet().size());
         }
         //  System.out.println("      segment.getPoint9TreeSet().size()  " + segment.getPoint9TreeSet().size());
         //System.out.println(segment);
